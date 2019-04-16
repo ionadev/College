d3bbea29f0431d015d216545fa2a37ce3ea6d1bb
Piyush <bhangalepiyush@gmail.com>
2019-04-16 12:40:18 +0530

    Finished!

--- OOP/Ratio.cpp
+++ OOP/Ratio.cpp
@@ -2,10 +2,38 @@
 
 class Ratio
 {
+	private: int numerator,denominator;
+	public:
+		void get()
+		{
+			cout << "Enter The Numerator: " << endl;
+			cin >> numerator;
+			cout << "Enter The Denominator: " << endl;
+			cin >> denominator;
+		}
 
+		void calculate()
+		{
+			float result, newNumerator;
+
+			newNumerator = (float)(numerator);
+
+			result = newNumerator / denominator;
+
+			cout << "The Division is: " << result << endl;
+		}
+
+		void reciprocal()
+		{
+			cout << "The Reciprocal Is: " << denominator << "/" << numerator;
+		}
 };
 
 int main()
 {
-	
+	Ratio r;
+
+	r.get();
+	r.calculate();
+	r.reciprocal();
 }
