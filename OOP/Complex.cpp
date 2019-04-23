3bff5badd3d952b9b725167dc95c080ab65db1d0
Piyush <bhangalepiyush@gmail.com>
2019-04-23 13:27:58 +0530

    Finished!

--- OOP/Complex.cpp
+++ OOP/Complex.cpp
@@ -19,3 +19,47 @@ using namespace std;
 // 			nn = (a1*d1) + (a)
 // 		}
 // }
+
+class Ratio
+{
+int num,den;
+public:
+	Ratio()
+	{
+		num = den = 0;
+	}
+
+	void get()
+	{
+		cout << "Enter The Numerator: ";
+		cin >> num;
+		cout << endl;
+
+		cout << "Enter The Denominator: ";
+		cin >> den;
+		cout << endl;
+	}
+
+	void show()
+	{
+		cout << "Num: " << num << " Den: " << den << endl;
+	}
+
+	Ratio operator +(Ratio t)
+	{
+		Ratio nR;
+		nR.num = (t.num * den) + (t.den * num);
+		nR.den = (t.den * den);
+
+		return(nR);
+	}
+};
+
+int main()
+{
+	Ratio o1,o2,o3;
+	o1.get();
+	o2.get();
+	o3 = o1 + o2;
+	o3.show();
+}
