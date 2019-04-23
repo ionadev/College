a32a7c77cafcdf9c4eeacbc898028583682fbe08
Piyush <bhangalepiyush@gmail.com>
2019-04-23 13:08:49 +0530

    Oki

---
+++ OOP/Complex.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// class AddFraction
+// {
+// 	private: int n1,n2,d1,d2,nn,nd;
+// 	public:
+// 		AddFraction()
+// 		{
+// 			n1 = n2= d1 = d2 = nn = nd = 0;
+// 		}
+// 		void add(int a1,a2,b1,b2)
+// 		{
+// 			n1 = a1;
+// 			n2 = a2;
+// 			d1 = b1;
+// 			d2 = b2;
+
+// 			nn = (a1*d1) + (a)
+// 		}
+// }
