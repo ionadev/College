f24bba85a5ca4cc6680922d84c3b7b9a36664dfd
Piyush <bhangalepiyush@gmail.com>
2019-07-02 12:11:22 +0530

    UwU New textbook question

---
+++ Previous/replace_hyphen.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void replace(char[]);
+
+int main()
+{
+    char a[80];
+    cin.getline(a, 81);
+    replace(a);
+    return 0;
+}
+
+void replace(char a[80])
+{
+	for (int i = 0; a[i] != '\0'; i++)
+	{
+		if(a[i] == ' ') {
+            a[i] = '-';
+        }
+	}
+
+	cout << a << endl;
+}
