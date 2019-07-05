cc30ee6505556d223a4032f265849bbc75b68095
Piyush <bhangalepiyush@gmail.com>
2019-07-05 11:36:01 +0530

    more ratio

---
+++ Practicals/ratio-conversion.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Ratio
+{
+    double numerator, denominator;
+    public:
+
+    void assign(double _numerator, double _denominator)
+    {
+        numerator = _numerator;
+        denominator = _denominator;
+        print();
+    }
+
+    void convert()
+    {
+        cout << "Conversion is: " << (numerator / denominator) << endl;
+    }
+
+    void invert()
+    {
+        double _numertator;
+        _numertator = numerator;
+        numerator = denominator;
+        denominator = _numertator;
+        print();
+    }
+
+    void print()
+    {
+        cout << "The Ratio is now: " << numerator << ":" << denominator << endl;
+    }
+};
+
+int main()
+{
+    Ratio r;
+    r.assign(12.0,13.0);
+    r.convert();
+    r.invert();
+}
